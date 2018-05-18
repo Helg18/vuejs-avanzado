61bae0beb2242c1c88a36bdb368d21226c0b130b
Henry Leon <helg18@gmail.com>
2018-05-17 22:52:47 -0400

    Quitando las palabras Platzi

--- src/components/layouts/Header.vue
+++ src/components/layouts/Header.vue
@@ -5,7 +5,7 @@
 				<div class="container">
 					<div class="nav-left">
 						<div class="nav-item">
-							<strong>Platzi Music</strong>
+							<strong>Music</strong>
 						</div>
 					</div>
 					<div class="nav-rigth nav-menu"></div>
@@ -14,7 +14,7 @@
 		</div>
 		<div class="hero-body">
 			<div class="container has-text-centered">
-				<h1>Platzi Music</h1>
+				<h1>Music</h1>
 				<h2>Canciones que estan de moda</h2>
 			</div>
 		</div>
